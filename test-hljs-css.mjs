27631822e49b80d4b336c53c903249c7865dad14
vehang <vehang@users.noreply.github.com>
2026-03-13 08:25:22 +0800

    chore: 清理测试文件

--- test-hljs-css.mjs
+++
@@ -1,8 +0,0 @@
-import githubDark from 'highlight.js/styles/github-dark.css?inline'
-import atomOneDark from 'highlight.js/styles/atom-one-dark.css?inline'
-
-console.log('=== GitHub Dark CSS (first 2000 chars) ===')
-console.log(githubDark.substring(0, 2000))
-console.log('')
-console.log('=== Atom One Dark CSS (first 2000 chars) ===')
-console.log(atomOneDark.substring(0, 2000))
